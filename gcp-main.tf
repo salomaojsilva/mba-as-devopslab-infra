73f7d92f18ef8987ef5c8052c253448ae4586358
salomaojsilva <salomao.jsilva@gmail.com>
2021-08-15 19:21:32 -0300

    Update gcp-main.tf

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,4 +1,10 @@
-resource "google_app_engine_application" "appSJS" {
-  project     = "lab-devops-cloud-322914"
+resource "google_project" "devopslab-SJS" {
+  name       = "devopslab-SJS"
+  project_id = "devopslab-SJS"
+  org_id     = "1234567"
+}
+
+resource "google_app_engine_application" "app" {
+  project     = "devopslab-SJS"
   location_id = "us-central"
 }
